15c0d1b456758eefca4d89c9db2e0a6b4afb7a3c
geovane-campos <geovane1250@gmail.com>
2024-12-01 19:01:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 let nome = "geovane"
-console.log(nome)
+let xp = 0
+let ferro = 999
+let bronze = 2000
+let prata = 5000
+let ouro = 7000
+let platina = 8000
+let ascendente = 9000
+let imortal = 10.000
+let radiante = 10.001
+  
+  for (let i = 0; i <= 998 ; i++){
+     xp += 1
+      
+  }
+  if(ferro <= 999 ){
+    xp = "ferro"
+    console.log("o heroi de nome " + nome +" está no nivel " + xp)
+  }
